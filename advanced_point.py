73408f41291bef95adad85d37930da1b9724139d
Kian <kianluis1382@gmail.com>
2025-05-02 12:10:15 +0200

    docstrings + comments

--- advanced_point.py
+++ advanced_point.py
@@ -9,7 +9,6 @@ class AdvancedPoint(ColorPoint):
 
     def __init__(self, x, y, color):
         """
-        Initialize an AdvancedPoint with coordinates and a valid color.
         :param x: x-coordinate (int or float)
         :param y: y-coordinate (int or float)
         :param color: string representing the color
@@ -21,46 +20,45 @@ class AdvancedPoint(ColorPoint):
         self._y = y
         self._color = color
 
-    @property  # shortcut for calling a function without needing to say p.x() instead calling it p.x
+    @property
     def x(self):
         """
-        Getter for x-coordinate.
+        :return: x-coordinate
         """
         return self._x
 
     @x.setter
     def x(self, value):
         """
-        Setter for x-coordinate.
+        :param value: new x-coordinate
         """
         self._x = value
 
     @property
     def y(self):
         """
-        Getter for y-coordinate.
+        :return: y-coordinate
         """
         return self._y
 
     @property
     def color(self):
         """
-        Getter for color.
+        :return: color of the point
         """
         return self._color
 
     @classmethod
     def add_color(cls, color):
         """
-        Adds a new valid color for our class.
         :param color: new color to add to the valid COLORS list
+        :return: None
         """
         cls.COLORS.append(color)
 
     @staticmethod
     def from_tuple(coordinate, color="red"):
         """
-        Create a new point from a tuple rather than 2 individual values.
         :param coordinate: a tuple (x, y)
         :param color: optional color string (default "red")
         :return: AdvancedPoint instance
@@ -71,7 +69,6 @@ class AdvancedPoint(ColorPoint):
     @staticmethod
     def distance_2_points(p1, p2):
         """
-        Static method to calculate distance between two points.
         :param p1: first point (AdvancedPoint)
         :param p2: second point (AdvancedPoint)
         :return: Euclidean distance as float
@@ -80,19 +77,19 @@ class AdvancedPoint(ColorPoint):
 
     def distance_to_other(self, p):
         """
-        Instance method to calculate distance to another point.
         :param p: another point
         :return: Euclidean distance as float
         """
         return ((self.x - p.x) ** 2 + (self.y - p.y) ** 2) ** 0.5
 
 
-AdvancedPoint.add_color("rojo")
+AdvancedPoint.add_color("rojo")  # Add a new valid color to the COLORS list
 p = AdvancedPoint(1, 2, "rojo")  # Create an instance with the new color
 print(p.x)
-print(p)  # Uses __str__ inherited from ColorPoint
-print(p.distance_orig())
-p2 = AdvancedPoint.from_tuple((3, 2))
+print(p)
+print(p.distance_orig())  # Uses inherited method from Point
+
+p2 = AdvancedPoint.from_tuple((3, 2))  # Create a point from a tuple
 print(p2)
 print(AdvancedPoint.distance_2_points(p, p2))  # Static method: distance between two points
-print(p.distance_to_other(p2))
+print(p.distance_to_other(p2))  # Instance method: distance to another point
